319116d022c0a323796e1eb7549b644383753438
BananaJeanss <18456310+BananaJeanss@users.noreply.github.com>
2025-08-25 11:56:35 +0300

    feat: added --port arg, added reading request until eol with \r\n\r\n

--- main.cpp
+++ main.cpp
@@ -8,14 +8,45 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <sys/stat.h>
+#include <signal.h>
+#include <cstdlib>
 
-int main()
+int main(int argc, char *argv[])
 {
     printf("faucet http server\n");
     printf("---\n");
 
-    // create the socket
+    int port = 8080;
 
+    // loop through args
+    for (int i = 1; i < argc; i++)
+    {
+        if (strcmp(argv[i], "--port") == 0 && i + 1 < argc)
+        {
+            // make sure port is no larger than 65535
+            int p = atoi(argv[i + 1]);
+            if (p < 1 || p > 65535)
+            {
+                printf("Invalid port number: %s\n", argv[i + 1]);
+                return 1;
+            }
+            port = atoi(argv[i + 1]);
+            i++;
+        }
+        else if (strcmp(argv[i], "--help") == 0 || strcmp(argv[i], "-h") == 0)
+        {
+            printf("Usage: %s [--port <port>] [--help]\n", argv[0]);
+            return 0;
+        }
+        else
+        {
+            printf("Unknown argument: %s\n", argv[i]);
+            printf("Usage: %s [--port <port>] [--help]\n", argv[0]);
+            return 1;
+        }
+    }
+
+    // create the socket
     int sock = socket(AF_INET, SOCK_STREAM, 0);
     if (sock == -1)
     {
@@ -28,7 +59,7 @@ int main()
     // struct for the bind
     sockaddr_in addr{};
     addr.sin_family = AF_INET;
-    addr.sin_port = htons(8080);
+    addr.sin_port = htons(port);
     addr.sin_addr.s_addr = INADDR_ANY;
 
     // bind
@@ -40,7 +71,7 @@ int main()
 
     char ip[INET_ADDRSTRLEN];
     inet_ntop(AF_INET, &addr.sin_addr, ip, sizeof(ip));
-    printf("\nListening on %s:%d\n", ip, ntohs(addr.sin_port));
+    printf("Listening on %s:%d\n", ip, ntohs(addr.sin_port));
     fflush(stdout);
 
     // listen
@@ -54,17 +85,39 @@ int main()
     for (;;)
     {
         int client_fd = accept(sock, 0, 0);
-
-        char buffer[256] = {0};
-        ssize_t n = recv(client_fd, buffer, sizeof(buffer) - 1, 0);
-        if (n <= 0)
+        if (client_fd < 0)
         {
-            perror("recv");
-            close(client_fd);
+            perror("accept");
             close(sock);
             return 1;
         }
-        buffer[n] = 0;
+
+        // read headers/request
+        char buffer[4096];
+        ssize_t used = 0;
+        const char *eolmark = "\r\n\r\n"; // until eol
+        while (used < (ssize_t)sizeof(buffer) - 1)
+        {
+            ssize_t recvd = recv(client_fd, buffer + used, sizeof(buffer) - 1 - used, 0);
+            if (recvd <= 0)
+            {
+                // error or closed
+                close(client_fd);
+            }
+            used += recvd;
+            buffer[used] = 0; // null terminate for strstr
+
+            if (strstr(buffer, eolmark))
+                break; // got all headers
+        }
+
+        // if header too large/malformed, close
+        if (!strstr(buffer, eolmark))
+        {
+            close(client_fd);
+            close(sock);
+            return 0;
+        }
 
         int requestIp = ntohl(addr.sin_addr.s_addr);
 
@@ -79,7 +132,6 @@ int main()
         {
             // unsupported method
             close(client_fd);
-            close(sock);
             return 0;
         }
 
